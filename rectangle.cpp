2cf40bc566e946c0bae4711f8c45f5a502576f46
Gabriel <gabrielsouto100d@gmail.com>
2014-10-09 17:34:53 +0100

    faltava os rect

--- rectangle.cpp
+++ rectangle.cpp
@@ -11,6 +11,12 @@ rectangle::rectangle(float x1,float y1,float x2,float y2)
 }
 
 
+float * rectangle:: getCoords() const
+{
+	float coords[] = {x1,y1,x2,y2};
+	return coords;
+}
+
 rectangle::~rectangle(void)
 {
 }
--- rectangle.h
+++ rectangle.h
@@ -4,6 +4,7 @@ class rectangle
 	float x1, y1, x2, y2;
 public:
 	rectangle(float x1,float y1,float x2,float y2);
+	float* getCoords() const;
 	~rectangle(void);
 };
 
