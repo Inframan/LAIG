67c9d39131f3964c2b46f712861663008e73bf49
Jose <josecardos@gmail.com>
2014-10-16 00:05:04 +0100

    parser done/starting text

--- myTorus.cpp
+++ myTorus.cpp
@@ -8,7 +8,6 @@ myTorus::myTorus(float inner, float outer, int slices, int loops)
 	this->slices = slices;
 	this->loops = loops;
 }
-
 void myTorus::draw()
 {
 	double pi = acos(-1.0);
@@ -20,36 +19,25 @@ void myTorus::draw()
 	double v = 0.0f;
 	double w = 0.0f;
 
-// outer loop
+	// outer loop
 	while (w < 2 * pi + dw) {
 		v = 0.0f;
 		glBegin(GL_TRIANGLE_STRIP);
 		// inner loop
 		while (v < 2 * pi + dv) {
-			glNormal3d(
-					(outer + rr * cos(v)) * cos(w)
-							- (outer + inner * cos(v)) * cos(w),
-					(outer + rr * cos(v)) * sin(w)
-							- (outer + inner * cos(v)) * sin(w),
-					(rr * sin(v) - inner * sin(v)));
-			glVertex3d((outer + inner * cos(v)) * cos(w),
-					(outer + inner * cos(v)) * sin(w), inner * sin(v));
-			glNormal3d(
-					(outer + rr * cos(v + dv)) * cos(w + dw)
-							- (outer + inner * cos(v + dv)) * cos(w + dw),
-					(outer + rr * cos(v + dv)) * sin(w + dw)
-							- (outer + inner * cos(v + dv)) * sin(w + dw),
-					rr * sin(v + dv) - inner * sin(v + dv));
-			glVertex3d((outer + inner * cos(v + dv)) * cos(w + dw),
-					(outer + inner * cos(v + dv)) * sin(w + dw),
-					inner * sin(v + dv));
+			double cosV = cos(v), sinV = sin(v), cosW = cos(w),sinW = sin(w),
+				cosVdV = cos(v + dv), sinVdV = sin(v+dv), cosWdW = cos(w+dw),sinWdW = sin(w+dw);
+
+			glNormal3d((outer + rr * cosV) * cosW - (outer + inner * cosV) * cosW, (outer + rr * cosV) * sinW - (outer + inner * cosV) * sinW,(rr * sinV - inner * sinV));
+			glVertex3d((outer + inner * cosV) * cosW,(outer + inner * cosV) * sinW, inner * sinV);
+			glNormal3d((outer + rr * cosVdV) * cosWdW - (outer + inner * cosVdV) * cosWdW, (outer + rr * cosVdV) * sinWdW - (outer + inner * cosVdV) * sinWdW, rr * sinVdV - inner * sinVdV);
+			glVertex3d((outer + inner * cosVdV) * cosWdW, (outer + inner * cosVdV) * sinWdW, inner * sinVdV);
 			v += dv;
 		} // inner loop
 		glEnd();
 		w += dw;
 	} //outer loop
 }
-   
 
 
 myTorus::~myTorus(void)
