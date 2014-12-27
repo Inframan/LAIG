c39e34b58fb0dfc2f2b29b3625f8a282dc29f3f2
Jose Cardoso <josecardos@gmail.com>
2014-12-27 18:35:54 +0000

    Started implementing sockets

---
+++ connectProlog.h
@@ -0,0 +1,21 @@
+#pragma once
+#include <winsock2.h>
+#include <iostream>
+
+#define IPADDRESS "127.0.0.1"
+#define PORT 60070
+
+using namespace std;
+
+SOCKET m_socket;
+
+
+bool socketConnect();
+
+void envia(char *s, int len);
+
+void recebe(char *ans);
+
+void quit();
+
+int startConnection();
